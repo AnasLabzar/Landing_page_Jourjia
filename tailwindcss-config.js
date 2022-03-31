4e4e1fffd2c5adb7ed3d76a0443ecc949b4d4b35
AnasLabzar <77747369+AnasLabzar@users.noreply.github.com>
2022-03-31 09:45:24 +0000

    First Version

---
+++ tailwindcss-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      'sans': ['HELVETICANEUELTARABIC-BOLD', 'sans-serif'],
+    },
+    extend: {}
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
